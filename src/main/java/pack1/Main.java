2d6e50fb3de9f2391eb53d84ecc5a6155a5e175a
9956799@gmail.com <Qqq_12345>
2021-09-16 20:16:36 +0300

    Java destructors!
    @Component
    public class TransctionPersistenceContextUserService {
        @PersistenceContext
        private EntityManager entityManager;
    
        @Transactional
        public User insertWithTransaction(User user) {
            entityManager.persist(user);
            return user;
        }
    
        public User insertWithoutTransaction(User user) {
            entityManager.persist(user);
            return user;
        }
    
        public User find(long id) {
            return entityManager.find(User.class, id);
        }
    }
    
    Спецификация JPA определяет несколько типов контекстов EntityManager / Persistence. Мы можем иметь:
    -расширенные и транзакционные EntityManager,
    -EntityManager, управляемый контейнером или приложением.
    -JTA или ресурс-локальный EntityManager,
    
    An EXTENDED persistence context can span across multiple transactions. We can persist the entity without the transaction but cannot flush it without a transaction.
    https://www.baeldung.com/jpa-hibernate-persistence-context
    https://www.sitepoint.com/cdi-weld-inject-jpa-hibernate-entity-managers/#creatinganinjectableentitymanager
    
    @PersistenceContext(type = PersistenceContextType.EXTENDED)
    private EntityManager entityManager;

--- src/main/java/pack1/Main.java
+++ src/main/java/pack1/Main.java
@@ -1,31 +1,93 @@
 package pack1;
 
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
 import pack1.Model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.inject.Inject;
+import javax.persistence.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main {
-	public static void main(String[] args) {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("def1");
-//		EntityManager entityManager = entityManagerFactory.createEntityManager();
-//		EntityTransaction transaction = entityManager.getTransaction();
+
+	final static String CFG_FILE = "local.properties";
+	final static Properties persistenceProperties = new Properties();
+
+	static {
+		InputStream is = null;
 
 		try {
-			//transaction.begin();
+			is = Main.class.getClassLoader().getResourceAsStream(CFG_FILE);
+			if (is == null)
+				throw new IOException(String.format("File %s not found... ", CFG_FILE));
+			persistenceProperties.load(is);
+		}
+		catch (IOException ex) {
+			System.out.println(ex.getMessage());
+			throw new RuntimeException(ex.getMessage());
+		}
+		finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException ignored) {
+				}
+			}
+		}
+	}
 
 
-//			User user = new User("user1");
-//			entityManager.persist(user);
+	@Produces
+	//@MySQLDatabase
+	public EntityManager createEntityManager() {
+		return Persistence
+				.createEntityManagerFactory("my-persistence-unit")
+				.createEntityManager();
+	}
 
-			//transaction.commit();
-		} finally {
+
+	@Inject
+	@PersistenceContext(unitName = "my-app", properties = persistenceProperties)
+	private EntityManager entityManager;
+
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManFactory = Persistence.createEntityManagerFactory("my-app", persistenceProperties);
+//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-app");
+
+		EntityManager entityManager = entityManFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+
+		Weld weld = new Weld();
+		WeldContainer container = weld.initialize();
+		//ImageFileProcessor imageFileProcessor = container.select(ImageFileProcessor.class).get();
+
+		//System.out.println(imageFileProcessor.openFile("file1.png"));
+
+		container.shutdown();
+
+		try {
+			transaction.begin();
+
+
+			User user = new User();
+			entityManager.persist(user);
+
+			transaction.commit();
+
+			int a = 1;
+		}
+		catch (Exception ex)
+		{
+			int a  = 2;
+		}
+		finally {
 //			if (transaction.isActive())
 //				transaction.rollback();
 //			entityManager.clear();
-			entityManagerFactory.close();
+			entityManFactory.close();
 		}
 	}
 }
